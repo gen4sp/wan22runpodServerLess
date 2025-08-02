7f3a1fcc2413ed2c08a3642369f8562ebb46e840
gen4 <gen4mail@gmail.com>
2025-08-02 14:22:21 -0300

    upd

--- rp_handler.py
+++ rp_handler.py
@@ -37,6 +37,7 @@ def encode_image_to_base64(image_path):
     with open(image_path, "rb") as image_file:
         return base64.b64encode(image_file.read()).decode('utf-8')
 
+# еу
 def upload_image_to_comfy(image_data, filename="input_image.png"):
     """Загружает изображение в ComfyUI"""
     try:
