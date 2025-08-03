4868c3040729e590afd461e3cd6f686bd7d0eddc
GitHub Action <action@github.com>
2025-08-03 00:10:45 +0000

    chore: bump version to 1.0.24

--- version.py
+++ version.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """Управление версиями для WAN 2.2 RunPod Worker."""
 
-__version__ = "1.0.23"
-__version_info__ = (1, 0, 23)
+__version__ = "1.0.24"
+__version_info__ = (1, 0, 24)
 
 def get_version():
     """Возвращает текущую версию."""
