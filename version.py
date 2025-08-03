afea6a3fdcea34c1c2fdd15ef4b4f1fdb1379b56
gen4 <gen4mail@gmail.com>
2025-08-03 04:30:17 -0300

    upd

--- version.py
+++ version.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """Управление версиями для WAN 2.2 RunPod Worker."""
 
-__version__ = "1.0.12"
-__version_info__ = (1, 0, 12)
+__version__ = "1.1.1"
+__version_info__ = (1, 1, 1)
 
 def get_version():
     """Возвращает текущую версию."""
